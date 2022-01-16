22737a46cc3df075806f66272cd2db0229f13ace
root <root@i-0074ea7461d22ce1a.ap-northeast-2.compute.internal>
2022-01-16 12:11:01 +0000

    add support for ARM-based architectures

--- ec2/build.sh
+++ ec2/build.sh
@@ -4,8 +4,13 @@
 docker build \
     -t 037884848543.dkr.ecr.ap-northeast-2.amazonaws.com/zai-ml-tracker:$1 .
 
-# Get AWS credentials
-aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 037884848543.dkr.ecr.ap-northeast-2.amazonaws.com
+# Get AWS credentials and push to ECR
+aws ecr get-login-password \
+    --region ap-northeast-2 | \
+    docker login \
+    --username AWS \
+    --password-stdin \
+    037884848543.dkr.ecr.ap-northeast-2.amazonaws.com && \
+    docker push \
+    037884848543.dkr.ecr.ap-northeast-2.amazonaws.com/zai-ml-tracker:$1
 
-# Push to ECR
-docker push 037884848543.dkr.ecr.ap-northeast-2.amazonaws.com/zai-ml-tracker:$1
